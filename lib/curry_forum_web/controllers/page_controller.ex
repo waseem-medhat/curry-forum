0fe39f92f3b7551abbb001ac1abe2e3df05791f8
wipdev-tech <wipdev.tech@gmail.com>
2024-05-05 11:32:33 -0400

    New route with mock data

--- lib/curry_forum_web/controllers/page_controller.ex
+++ lib/curry_forum_web/controllers/page_controller.ex
@@ -6,4 +6,15 @@ defmodule CurryForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    IO.puts("users page yay")
+
+    users = [
+      %{id: 1, name: "Hamada", email: "hamada@hmd.com"},
+      %{id: 2, name: "Msh", email: "mshmsh@hmd.com"}
+    ]
+
+    render(conn, :users, users: users, layout: false)
+  end
 end
